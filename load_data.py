de3df309515e4482e67e446a120515068af783a5
Roman Pavelka <roman.pavelka.asi@gmail.com>
2021-12-14 05:55:21 +0100

    Milesovka v temperature8.py

--- load_data.py
+++ load_data.py
@@ -1,10 +1,18 @@
 import numpy as np
 from datetime import datetime as dt
 
-fn = "klementinum.csv"
+KLEMENTINUM = "klementinum.csv"
+MILESOVKA = "milesovka.csv"
 
 
-def load_data():
+def load_data(dataset=KLEMENTINUM):
+    if dataset == KLEMENTINUM:
+        return load_klementinum()
+    if dataset == MILESOVKA:
+        return load_milesovka()
+
+def load_klementinum():
+    fn = KLEMENTINUM
     with open(fn) as f:
         header = f.readline()
     header = [l.strip() for l in header.split(",")]
@@ -18,8 +26,34 @@ def load_data():
     return header[3:7], dates, data[:, 3:7]
 
 
+def load_milesovka():
+    fn = MILESOVKA
+    data = np.genfromtxt(fn, delimiter=',', skip_header=1)
+
+    N_rows = data.shape[0]
+
+    dates = []
+    Tavg = []
+    for i in range(N_rows):
+        row = data[i]
+        year = int(row[0])
+        month = int(row[1])
+        for day in range(1, 32):
+            value = row[1 + day]
+            if np.isnan(value):
+                break
+            d = dt(year, month, day)
+            dates.append(d)
+            Tavg.append(value)
+            #print(f"{d} {value}")
+
+    return np.array(dates), np.array(Tavg)
+
+
 def view_array(x, n=3):
     L = x.shape[0]
+    M = x.shape[1]
+    print(f"{L}x{M}")
     [print(x[i]) for i in range(n)]
     print("...")
     [print(x[L-n+i]) for i in range(n)]
---
+++ temperature7.py
@@ -0,0 +1,35 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+
+from load_data import load_data
+from moving_average import moving_average
+
+markersize = .1
+N = 365
+
+if __name__ == "__main__":
+    plt.suptitle(
+        "Roční a desetileté průměrné teplot\n"
+        "Data: ČHMÚ, Zpracování: Roman Pavelka")
+    plt.ylabel("$\mathrm{T_{avg} [°C]}$")
+
+    # Klementinum
+    header, dates, data = load_data()
+    avgT, maxT, minT = data[:,0], data[:,1], data[:,2]
+    plt.plot(dates, moving_average(avgT, N), 'r', lw=.1)
+    plt.plot(dates, moving_average(avgT, 10*N), 'r', lw=1, label="Klementinum")
+
+    avg = np.mean(avgT)
+    plt.plot([dates[0], dates[-1]], [avg, avg], 'r--', lw=.8)
+
+    # Milesovka
+    dates_m, avgT_m = load_data("milesovka.csv")
+    plt.plot(dates_m, moving_average(avgT_m, N), 'b', lw=.1)
+    plt.plot(dates_m, moving_average(avgT_m, 10*N), 'b', lw=1, label="Milešovka")
+
+    avg_m = np.mean(avgT_m)
+    plt.plot([dates_m[0], dates_m[-1]], [avg_m, avg_m], 'b--', lw=.8)
+
+    plt.legend()
+    plt.grid()
+    plt.show()
